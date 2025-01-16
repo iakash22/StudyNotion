7573610d5df8d67f45dc7bc096e3a649450a3c6b
Akash kumar <94979388+iakash22@users.noreply.github.com>
2025-01-17 03:33:14 +0530

    add Edit and my Course route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,8 @@ import { ACCOUNT_TYPE } from './utils/Constants'
 import EnrolledCourses from './components/core/Dashboard/EnrolledCourses'
 import Cart from './components/core/Dashboard/cart'
 import AddCourse from './components/core/Dashboard/AddCourse';
-import MyCourse from './components/core/Dashboard/MyCourse';
+import MyCourses from './components/core/Dashboard/MyCourses';
+import EditCourse from './components/core/Dashboard/EditCourse';
 
 function App() {
 
@@ -89,7 +90,8 @@ function App() {
             {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
               <>
                 <Route path='dashboard/add-course' element={<AddCourse />} />
-                <Route path='dashboard/my-courses' element={<MyCourse />} />
+                <Route path='dashboard/my-courses' element={<MyCourses />} />
+                <Route path='dashboard/edit-course/:courseId' element={<EditCourse />} />
               </>
             )}
           </Route>
