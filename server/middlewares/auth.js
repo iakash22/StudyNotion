d17e7821f684ea9b9ce7e11b5ea21f486b2210a5
Akash kumar <94979388+iakash22@users.noreply.github.com>
2025-01-17 03:40:23 +0530

    update auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,7 +6,7 @@ exports.auth = async (req, res, next) => {
     try {
         const token = req.cookies?.token
             || req.body?.token
-            || req.header("Authorisation")?.replace("Bearer ", "");
+            || req.header("Authorization")?.replace("Bearer ", "");
             
             if (!token) {
                 return res.status(401).json({
